3e99421f7c525dad0275d32864a233e823a2a15c
Wonmi <wonmadesign@gmail.com>
2018-12-26 18:07:59 +0900

    Updates

---
+++ precache-manifest.9a4f17b3dce45d90200a577fd4589e09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "c2aeacf0cccfe92ff60a",
+    "url": "/robofriends/static/js/main.c2aeacf0.chunk.js"
+  },
+  {
+    "revision": "a81680702c5fd084baa9",
+    "url": "/robofriends/static/js/1.a8168070.chunk.js"
+  },
+  {
+    "revision": "c2aeacf0cccfe92ff60a",
+    "url": "/robofriends/static/css/main.624116ca.chunk.css"
+  },
+  {
+    "revision": "a81680702c5fd084baa9",
+    "url": "/robofriends/static/css/1.06e831d4.chunk.css"
+  },
+  {
+    "revision": "ebd538ab16d1a9c9066e21c7b16985c4",
+    "url": "/robofriends/index.html"
+  }
+];
